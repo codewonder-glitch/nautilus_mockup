00d8e10a5717de3d078fdba25622159f13d62c04
Visa <visalakshi.cse@gmail.com>
2020-04-03 01:23:37 -0500

    Included all the images

--- nautilus/src/components/Matches.js
+++ nautilus/src/components/Matches.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import matchesimg from  '../assets/matches.png'
-
+import matchesimg from  '../assets/bio_and_beyond.jpeg'
+import wiseimg from  '../assets/wise_sci_and_engineering.png'
+import abstractimg from  '../assets/abstractions.jpeg'
+import earthimg from   '../assets/earth.jpeg'
+import tlas from '../assets/tlas.jpeg'
+import cosmos from '../assets/cosmos.jpeg'
+import poetry from '../assets/poetry.jpeg'
+import maxplank from '../assets/maxplanck.jpeg'
+import aging from '../assets/aging.png'
+import alliance from '../assets/alliance.png'
+import './Matches.css'
 class Matches extends React.Component{
 constructor(props)
 {
@@ -8,24 +17,24 @@ constructor(props)
     super(props);
     this.state ={scroll
         :false}
-    this.handleScroll=this.handleScroll.bind(this);
+    
 }
  
-handleScroll = e => {
-    let element = e.target
-    if (element.scrollHeight - element.scrollTop === element.clientHeight) {
-      // do something at end of scroll
-      this.setState({scroll:true});
-    }
-  }
+
     render(){
 return(
 <div>
+   <img id="bigandbeyond" src={matchesimg}/>
+   <img src={wiseimg} />
+   <img src={abstractimg} />
+   <img src={earthimg} />
+   <img src={tlas} />
+   <img src={cosmos} />
+   <img src={poetry} />
+   <img src={aging} />
+   <img src={alliance} />
+   <img src={maxplank} />
     
-    if {!this.state.scroll }
-
-    <img id="matchimg" src={matchesimg} onScroll={this.handleScroll}/>
-   
 </div>
 
 );
